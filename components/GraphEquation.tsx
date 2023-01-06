ba5896e6005afb620ca5475eb24ec598327f2b5c
Josh Pullen <JoshPullen27@gmail.com>
2023-01-05 20:11:36 -0500

    Remove unused components

--- components/GraphEquation.tsx
+++
@@ -1,8 +0,0 @@
-import dynamic from "next/dynamic";
-
-const GraphEquation = dynamic(
-  () => import("./GraphEquationInternal").then((mod) => mod.GraphEquation),
-  { ssr: false }
-);
-
-export { GraphEquation };
